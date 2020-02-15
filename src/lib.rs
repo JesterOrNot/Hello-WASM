418c440d04db0c43334bf9347ee133103c03fefc
Sean Hellum <seanhellum45@gmail.com>
2020-02-15 21:08:05 +0000

    make it parse markdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+    #[wasm_bindgen(js_namespace = document)]
+    fn write(s: &str);
+    #[wasm_bindgen(js_namespace = window)]
+    fn prompt(s: &str) -> String;
 }
 
+// #[wasm_bindgen(start)]
+// pub fn main() {
+//     write("<h1>Hello World From Rust</h1>")
+// }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello World From Rust!");
-    log("LOGGING!")
+    log("LOGGING!");
+    let s = prompt("Hello");
+    let x = mossy::App::exec(String::from(&s));
+    write(x.as_str());
 }
