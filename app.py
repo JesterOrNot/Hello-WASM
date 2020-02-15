ebd589e9eb96a3d9081ce407c4cc4ab145d7ed0b
Restyled.io <commits@restyled.io>
2020-02-15 19:29:07 +0000

    Restyled by black

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
 
 
-@app.route('/')
+@app.route("/")
 def root():
-    return open('index.html').read()
+    return open("index.html").read()
 
 
 app.run(port=3000)
